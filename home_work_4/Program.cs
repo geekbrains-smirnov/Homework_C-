1851f3b174e18620bc7aec568a18e10686e4e0f4
Artyom Smirnov <expertvideo.as@gmail.com>
2022-06-28 23:53:35 +0300

    add home_work_4

---
+++ home_work_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int even = 1;
+int num = 0;
+
+while (count <= N)
+{
+    if (num == even % 2)
+    {
+        num = 0;
+        Console.WriteLine($"Четные числа: {even}");
+    }
+
+    count++;
+    even++;
+}
+
