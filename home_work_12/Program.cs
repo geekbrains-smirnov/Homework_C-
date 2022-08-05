3f5df066e08485b9755fb89f79473514fa40fb6c
Aryom Smirnov <expertvideo.as@gmail.com>
2022-08-05 15:51:32 +0300

    add home_work_12

---
+++ home_work_12/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.Write("Введите цельное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int MySum(int N)
+{
+    int result = 0;
+    while (N > 0)
+    {
+        
+        result = result + (N % 10);
+        N = N / 10;
+        
+    }
+    return result;
+}
+
+Console.WriteLine(MySum(N));
+
