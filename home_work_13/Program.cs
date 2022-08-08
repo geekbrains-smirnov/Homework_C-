f915a0c55f86d1b6a4b98be854961e769ea659ec
Aryom Smirnov <expertvideo.as@gmail.com>
2022-08-08 10:28:01 +0300

    add homework_13

---
+++ home_work_13/Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        
+        Console.WriteLine("["+ String.Join(", ", array) +  "]");
+        
+    }
+    Console.WriteLine();
+
+}
+Console.Write("Введите длину массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] myArr = new int[n];
+
+    for (int i = 1; i<= myArr.Length; i++)
+    {
+        Console.Write("Введите элементы массива: ");
+        myArr[i-1] = int.Parse(Console.ReadLine());
+
+    }
+
+PrintArray(myArr);
