e7f2b749e9002422d3f18ba2e512baa171882716
Artem Smirnov <expertvideo.as@gmail.com>
2022-08-30 15:36:50 +0300

    add home_work_25

---
+++ home_work_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.Write("Введите натуральное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string Number(int m, int n)
+{
+    if (m<=n) return Number(m, n-1) + $"{n}, ";
+    else return string.Empty; 
+}
+
+Console.WriteLine(Number(m,n));
